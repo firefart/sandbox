02efcadf3529f31ac301a5deb88c6de8d7197e3b
Christian Mehlmauer <firefart@gmail.com>
2020-07-01 17:23:05 +0200

    update

--- downloadFiles.ps1
+++ downloadFiles.ps1
@@ -20,7 +20,7 @@ Function Download-File {
 
 Try {
   Download-File -url "https://update.code.visualstudio.com/latest/win32-x64-user/stable" -path "$($PSScriptRoot)\scripts\setups\vscode.exe"
-  Download-File -url "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.6/npp.7.8.6.Installer.x64.exe" -path "$($PSScriptRoot)\scripts\setups\notepad++.exe"
+  Download-File -url "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.8/npp.7.8.8.Installer.x64.exe" -path "$($PSScriptRoot)\scripts\setups\notepad++.exe"
   Download-File -url "https://download.sublimetext.com/Sublime%20Text%20Build%203211%20x64%20Setup.exe" -path "$($PSScriptRoot)\scripts\setups\sublime.exe"
   Download-File -url "https://corretto.aws/downloads/latest/amazon-corretto-11-x64-windows-jdk.msi" -path "$($PSScriptRoot)\scripts\setups\corretto.msi"
   Download-File -url "https://ghidra-sre.org/ghidra_9.1.2_PUBLIC_20200212.zip" -path "$($PSScriptRoot)\scripts\setups\ghidra.zip"
@@ -28,15 +28,15 @@ Try {
   Download-File -url "https://sourceforge.net/projects/dosbox/files/latest/download" -path "$($PSScriptRoot)\scripts\setups\dosbox.exe"
   Download-File -url "https://sourceforge.net/projects/x64dbg/files/latest/download" -path "$($PSScriptRoot)\scripts\setups\x64dbg.zip"
   Download-File -url "https://download.sysinternals.com/files/SysinternalsSuite.zip" -path "$($PSScriptRoot)\scripts\setups\sysinternals.zip"
-  Download-File -url "https://github.com/0xd4d/dnSpy/releases/download/v6.1.4/dnSpy-net472.zip" -path "$($PSScriptRoot)\scripts\setups\dnSpy.zip"
+  Download-File -url "https://github.com/0xd4d/dnSpy/releases/download/v6.1.5/dnSpy-net472.zip" -path "$($PSScriptRoot)\scripts\setups\dnSpy.zip"
   Download-File -url "https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe" -path "$($PSScriptRoot)\scripts\setups\python3.exe"
   Download-File -url "https://www.python.org/ftp/python/2.7.18/python-2.7.18.amd64.msi" -path "$($PSScriptRoot)\scripts\setups\python2.msi"
   Download-File -url "https://github.com/horsicq/DIE-engine/releases/download/1.01/DIE_1.01_win.zip" -path "$($PSScriptRoot)\scripts\setups\detectiteasy.zip"
   Download-File -url "https://gitlab.com/x0r19x91/autoit-extractor/-/raw/master/net40/AutoIt%20Extractor/bin/x64/Release/AutoIt%20Extractor.exe?inline=false" -path "$($PSScriptRoot)\scripts\setups\autoitextractor.exe"
   Download-File -url "https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win64.zip" -path "$($PSScriptRoot)\scripts\setups\upx.zip"
   Download-File -url "https://mh-nexus.de/downloads/HxDSetup.zip" -path "$($PSScriptRoot)\scripts\setups\hxd.zip"
-  Download-File -url "https://nmap.org/npcap/dist/npcap-0.9991.exe" -path "$($PSScriptRoot)\scripts\setups\npcap.exe"
-  Download-File -url "https://1.eu.dl.wireshark.org/win64/Wireshark-win64-3.2.3.exe" -path "$($PSScriptRoot)\scripts\setups\wireshark.exe"
+  Download-File -url "https://nmap.org/npcap/dist/npcap-0.9994.exe" -path "$($PSScriptRoot)\scripts\setups\npcap.exe"
+  Download-File -url "https://1.eu.dl.wireshark.org/win64/Wireshark-win64-3.2.4.exe" -path "$($PSScriptRoot)\scripts\setups\wireshark.exe"
 }
 Catch {
   $error[0] | Format-List * -force
